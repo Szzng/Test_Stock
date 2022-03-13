c63913de5a537aa2b458bb4704b048b150118723
Szzng <seungheeseo53@gmail.com>
2022-03-13 13:46:59 +0900

    Update models.py

--- mynews/models.py
+++ mynews/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 
 class Keyword(models.Model):
     keyword = models.CharField(max_length=300, primary_key=True)
-    owner = models.ManyToManyField(User)
+    owner = models.ManyToManyField(get_user_model())
 
     def __str__(self):
         return self.keyword
